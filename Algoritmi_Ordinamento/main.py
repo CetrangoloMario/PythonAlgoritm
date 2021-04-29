1554dff35f2822ad1d90de301dd8af9816f6ea64
Mario Cetrangolo <55892147+CetrangoloMario@users.noreply.github.com>
2021-04-29 22:05:55 +0200

    add quick_sort.py

--- Algoritmi_Ordinamento/main.py
+++ Algoritmi_Ordinamento/main.py
@@ -2,6 +2,7 @@ from Algoritmi_Ordinamento.bubble_sort import *
 from Algoritmi_Ordinamento.naive_sort import *
 from Algoritmi_Ordinamento.insert_sort import *
 from Algoritmi_Ordinamento.merge_sort import *
+from Algoritmi_Ordinamento.quick_sort import *
 
 list=[1,42,2134,2,2,31,243,12,3112]
 size= len(list)
@@ -9,7 +10,8 @@ print(list)
 #naiveSort(list,size)
 #bubble_sort(list,size)
 #insertSort(list,size)
-mergesort(list)
+#mergesort(list)
 
+quickSort(list, 0,len(list)-1)
 
 print(list)
